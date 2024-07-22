006e48f69e8b313585c22947e2dbc0ea9209086f
Lucas-Henschel <lucas101henschel@gmail.com>
2024-07-21 21:26:56 -0300

    Implemented send messages to whatsapp

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const axios = require('axios');
 
 const client = new Client({
   authStrategy: new LocalAuth()
 });
+
+const messageData = {
+  messaging_product: "whatsapp",
+  recipient_type: "individual",
+  to: process.env.WHATSAPP_NUMBER,
+  type: "interactive",
+  interactive: {
+    type: "button",
+    body: {
+      text: "Seja bem-vindo(a) à Barbearia TechNosDev. Estamos aqui para oferecer o melhor serviço e cuidar do seu visual. Como podemos ajudar você hoje?"
+    },
+    action: {
+      buttons: [
+        {
+          type: "reply",
+          reply: {
+            id: "corte_cabelo",
+            title: "✂️ Corte de cabelo"
+          }
+        },
+        {
+          type: "reply",
+          reply: {
+            id: "barba_bigode",
+            title: "🧔 Barba e bigode"
+          }
+        },
+        {
+          type: "reply",
+          reply: {
+            id: "tratamentos_capilares",
+            title: "💈 Tratamentos"
+          }
+        }
+      ]
+    }
+  }
+};
 
 client.on('ready', () => {
   console.log('Client is ready!');
@@ -14,22 +53,20 @@ client.on('qr', qr => {
 });
 
 client.on('message', async message => {
-  let hello_msg = "Mensagem padrão";
   let msg = message.body.toLowerCase().trim();
-
   console.log("Mensagem recebida: " + msg);
 
-  if (msg.includes("teste")) {
-    if (msg.startsWith('teste ')) {
-      const parametro = msg.slice(6).trim();
-
-      let resposta = `Exemplo de resposta para ${parametro}`;
-      await message.reply(resposta);
-    } else {
-      await message.reply(hello_msg);
-    }
-  } else {
-    await message.reply(hello_msg);
+  if (msg.includes("agendamento")) {
+    axios.post(`https://graph.facebook.com/v19.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`, messageData, {
+      headers: {
+        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      console.log('Message sent successfully:', response.data);
+    }).catch(error => {
+      console.error('Error sending message:', error.response.data);
+    });
   }
 });
 
